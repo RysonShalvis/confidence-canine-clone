a0e0e8147a64f054e05791d9c1e8acc2e16581da
RysonShalvis <rysonshalvis@gmail.com>
2023-07-27 19:17:31 -0600

    add structure to homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import '../scss/App.scss';
-import '../scss/reset.scss';
+import '../scss/_app.scss';
+import '../scss/_reset.scss';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage/>
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import backgroundVideo from '../media/dog-wind-homepage.mp4';
+import logo from '../media/canine-logo.jpeg'
+import '../scss/_homepage.scss';
+
+function HomePage() {
+  return (
+    <div className='homepage'>
+
+      <div className='background-video'>
+        <video muted autoPlay loop src={backgroundVideo}/>
+      </div>
+
+      <div className='side-nav'>
+        <h2>TRAINING</h2>
+        <h2>PROGRAM</h2>
+        <h2>REVIEWS</h2>
+        <h2>CONTACT</h2>
+      </div>
+
+      <div className='main-content'>
+        <div>
+            <h2>OWNER CONFIDENCE</h2>
+            <h2>LEADERSHIP</h2>
+        </div>
+        <div className='logo-container'>
+            <img src={logo}/>
+        </div>
+        <div>
+            <h2>CANINE CONFIDENCE</h2>
+            <h2>TEAMWORK</h2>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default HomePage;
