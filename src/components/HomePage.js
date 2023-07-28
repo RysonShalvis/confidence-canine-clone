98445b8f17121434f39ce795b6f0b4d1d2b64de4
RysonShalvis <rysonshalvis@gmail.com>
2023-07-27 23:19:41 -0600

    add first quote section

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import backgroundVideo from '../media/dog-wind-homepage.mp4';
-import logo from '../media/canine-logo.jpeg'
-import '../scss/_homepage.scss';
+import React from "react";
+import backgroundVideo from "../media/dog-wind-homepage.mp4";
+import logo from "../media/canine-logo.jpeg";
+import "../scss/_homepage.scss";
 
 function HomePage() {
   return (
-    <div className='homepage'>
-
-      <div className='background-video'>
-        <video muted autoPlay loop src={backgroundVideo}/>
+    <div className="homepage">
+      <div className="background-video">
+        <video muted autoPlay loop src={backgroundVideo} />
       </div>
 
-      <div className='background-gradient'></div>
+      <div className="background-gradient"></div>
 
-      <div className='side-nav'>
+      <div className="side-nav">
         <h2>TRAINING</h2>
         <h2>PROGRAM</h2>
         <h2>REVIEWS</h2>
         <h2>CONTACT</h2>
       </div>
 
-      <div className='main-content'>
-        <div className='left-side-logo'>
-            <h2>OWNER CONFIDENCE</h2>
-            <h2>LEADERSHIP</h2>
+      <div className="main-content">
+        <div className="left-side-logo">
+          <h2>OWNER CONFIDENCE</h2>
+          <h2>LEADERSHIP</h2>
         </div>
-        <div className='logo-container'>
-            <img src={logo}/>
+        <div className="logo-container">
+          <img src={logo} />
         </div>
-        <div>
-            <h2>CANINE CONFIDENCE</h2>
-            <h2>TEAMWORK</h2>
+        <div className="right-side-logo">
+          <h2>CANINE CONFIDENCE</h2>
+          <h2>TEAMWORK</h2>
         </div>
       </div>
-
     </div>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,15 @@ import React from "react";
 import "../scss/_section.scss";
 
 const Section = () => {
-  return <div className="section"></div>;
+  return (
+    <div className="section">
+      <div className="section-quote-container">
+        <p>"WHATEVER IS WORTH DOING</p>
+        <p>IS WORTH DOING WELL."</p>
+        <p>Philip Stanhope</p>
+      </div>
+    </div>
+  );
 };
 
 export default Section;
